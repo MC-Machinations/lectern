00f47aec22754f552218ba3c1a8736bd13a65766
Jake Potrebic <jake.m.potrebic@gmail.com>
2023-06-11 13:56:43 -0700

    fix: add missing pom description

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
 plugins {
+    `maven-publish`
+    signing
     id("net.kyori.indra") version "2.0.6"
     id("net.kyori.indra.checkstyle") version "2.0.6"
     id("net.kyori.indra.license-header") version "2.0.6"
-    id("net.kyori.indra.publishing") version "2.0.6" apply false
 }
 
 group = "me.machinemaker"
@@ -32,7 +33,8 @@ allprojects {
 }
 
 subprojects {
-    apply(plugin="net.kyori.indra.publishing")
+    apply(plugin="maven-publish")
+    apply(plugin="signing")
 
     repositories {
         mavenCentral()
@@ -47,8 +49,8 @@ subprojects {
 
     indra {
         javaVersions {
-            target(16)
-            testWith(16)
+            target(17)
+            testWith(17)
         }
 
         github("MC-Machinations", "lectern") {
@@ -56,23 +58,94 @@ subprojects {
         }
 
         gpl3OnlyLicense()
+    }
 
-        publishReleasesTo("central", "https://oss.sonatype.org/service/local/staging/deploy/maven2")
-        publishSnapshotsTo("snapshots", "https://oss.sonatype.org/content/repositories/snapshots")
+    fun version(): String {
+        return rootProject.version.toString()
+    }
 
-        configurePublications {
-            pom {
-                developers {
-                    developer {
-                        id.set("Machine_Maker")
-                        email.set("machine@machinemaker.me")
-                    }
+    val isSnapshot = version().endsWith("-SNAPSHOT")
+
+    fun MavenPublication.standardConfig(versionName: String) {
+        group = project.group
+        artifactId = project.name
+        version = versionName
+
+        from(components["java"])
+
+        withoutBuildIdentifier()
+        pom {
+            val repoPath = "MC-Machinations/lectern"
+            val repoUrl = "https://github.com/$repoPath"
+
+            name.set(project.name)
+            url.set(repoUrl)
+            description.set(rootProject.description)
+            inceptionYear.set("2022")
+            packaging = "jar"
+
+            licenses {
+                license {
+                    name.set("GNU General Public License Version 3.0")
+                    url.set("https://www.gnu.org/licenses/gpl-3.0.en.html")
+                    distribution.set("repo")
                 }
+            }
+
+            issueManagement {
+                system.set("GitHub")
+                url.set("$repoUrl/issues")
+            }
+
+            developers {
+                developer {
+                    id.set("Machine_Maker")
+                    name.set("Jake Potrebic")
+                    email.set("machine@machinemaker.me")
+                    url.set("https://github.com/Machine-Maker")
+                }
+            }
+
+            scm {
+                url.set(repoUrl)
+                connection.set("scm:git:$repoUrl.git")
+                developerConnection.set("scm:git:git@github.com:$repoPath.git")
+            }
+        }
+
+    }
+
+    publishing {
+
+        publications {
+            register<MavenPublication>("maven") {
+                standardConfig(version())
+            }
+        }
+
+        repositories {
+            val url = if (isSnapshot) {
+                "https://oss.sonatype.org/content/repositories/snapshots"
+            } else {
+                "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
+            }
+            maven(url) {
+                name = "ossrh"
+                credentials(PasswordCredentials::class)
             }
         }
     }
 
+    signing {
+        useGpgCmd()
+        sign(publishing.publications["maven"])
+    }
+
     tasks {
+        withType<PublishToMavenRepository> {
+            dependsOn(check)
+        }
+
         test {
             useJUnitPlatform()
         }
